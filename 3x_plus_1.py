2dd2282d878f206117851ab44688e167909fed51
RandomGamingDev <83996185+RandomGamingDev@users.noreply.github.com>
2022-04-23 23:05:54 -0400

    Update 3x_plus_1.py

--- 3x_plus_1.py
+++ 3x_plus_1.py
@@ -1,12 +1,12 @@
 while True:
     number = int(input("Input your number: "))
-    i = 1;
+    i = 0;
     while number != 1:
         if number % 2:
             number*=3
             number+=1
         else:
             number/=2
-        print(f"{i}: {number}")
         i+=1
+        print(f"{i}: {number}")
     print(f"It took {i} steps to get to 1")
